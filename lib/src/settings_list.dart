676144cfd0798c435a3fa1db57328abdf49dda0b
Aanu Olakunle <olakunleaanu@gmail.com>
2020-04-07 15:45:53 +0100

    removed theme background color

--- lib/src/settings_list.dart
+++ lib/src/settings_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:settings_ui/src/colors.dart';
 import 'package:settings_ui/src/settings_section.dart';
 
 class SettingsList extends StatelessWidget {
@@ -9,29 +8,24 @@ class SettingsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).brightness == Brightness.light
-          ? backgroundGray
-          : Colors.black,
-      child: ListView.builder(
-        itemCount: sections.length,
-        itemBuilder: (context, index) {
-          SettingsSection current = sections[index];
-          SettingsSection futureOne;
-          try {
-            futureOne = sections[index + 1];
-          } catch (e) {}
+    return ListView.builder(
+      itemCount: sections.length,
+      itemBuilder: (context, index) {
+        SettingsSection current = sections[index];
+        SettingsSection futureOne;
+        try {
+          futureOne = sections[index + 1];
+        } catch (e) {}
 
-          // Add divider if title is null
-          if (futureOne != null && futureOne.title != null) {
-            current.showBottomDivider = false;
-            return current;
-          } else {
-            current.showBottomDivider = true;
-            return current;
-          }
-        },
-      ),
+        // Add divider if title is null
+        if (futureOne != null && futureOne.title != null) {
+          current.showBottomDivider = false;
+          return current;
+        } else {
+          current.showBottomDivider = true;
+          return current;
+        }
+      },
     );
   }
 }
